ec1cdec2c14ef3c28b6e3c436a4f0d3101a1d53c
Justin Fung <citrusvanilla@gmail.com>
2017-10-26 20:26:58 -0700

    progress: commiting mvp for database building script

--- customs_db.py
+++ customs_db.py
@@ -1,68 +1,89 @@
+##
+##  JFK Customs Simulation
+##  customs_db.py
+##
+##  Created by Justin Fung on 10/22/17.
+##  Copyright 2017 Justin Fung. All rights reserved.
+##
+## ====================================================================
+
+"""
+Builds an SQLite database to local disk to hold program data.
+
+Usage:
+  Please see the README for how to compile the program and run the
+  model and CSV formatting requirements.
+"""
 import sqlite3
+import pandas
 
 # Customs Database Metadata:
-sqlite_file = "customs_dg.sqlite"
+sqlite_file = "customs_db.sqlite"
 
-arrivals_table = {'name': 'arrivals',
-                  'fields': ['id',
-                  			 'arrival_time',
-                  			 'passengers_dom',
-                  			 'passengers_intl'],
-                  'types': ['INTEGER',
-                  			'TEXT',
-                  			'INTEGER',
-                  			'INTEGER']
-                 }
+# Schedule filepaths.
+arrival_schedule_csvfile = "schedules/sample_arrival_schedule.csv"
+server_schedule_csvfile = "schedules/sample_server_schedule.csv"
 
-servers_table = {'name': 'servers',
-				 'fields': ['id',
-				 			'subsection',
-				 			'0000-0400',
-				 			'0400-0800',
-				 			'0800-1200',
-				 			'1200-1600',
-				 			'1600-2000',
-				 			'2000-2400'],
-				'types': ['INTEGER',
-				 		  'TEXT',
-				 		  'INTEGER',
-				 		  'INTEGER',
-				 		  'INTEGER',
-				 		  'INTEGER',
-				 		  'INTEGER',
-				 		  'INTEGER']
-				}
 
-# Create Database by opening a connection for the first time.
-connection = sqlite3.connect(sqlit_file)
-cursor = connection.cursor()
+def build(sqlite_file, arrivals_csv, servers_csv):
+  """
+  Function that builds an initialized SQLite database file to disk.
 
-# Create new SQLite tables.
-arrivals_table_create_query = ('CREATE TABLE arrivals ('
-								 'id integer PRIMARY KEY, '
-								 'arrival_time text, '
-								 'passengers_dom integer, '
-								 'passengers_intl integer, '
-								 'table_constraint) '
-								 '[WITHOUT ROWID];')
+  Args:
 
-servers_table_create_query = ('CREATE TABLE servers ('
-							    'id integer PRIMARY KEY, '
-								'subsection text, '
-								'\'0-4\' integer, '
-								'\'4-8\' integer, '
-								'\'8-12\' integer, '
-								'\'12-16\' integer, '
-								'\'16-20\' integer, '
-								'\'20-24\' integer, '
-								'table_constraint) '
-								 '[WITHOUT ROWID];')
+  Returns:
+    VOID
+  """
+  # Create Database by opening a connection for the first time.
+  connection = sqlite3.connect(sqlite_file)
+  cursor = connection.cursor()
 
-cursor.execute(arrivals_table_create_query)
-cursor.execute(servers_table_create_query)
+  # Define Metadata for the tables.
+  arrivals_table_create_query = ('CREATE TABLE arrivals ('
+				  				   'id integer PRIMARY KEY, '
+								   'arrival_time text, '
+								   'passengers_dom integer, '
+								   'passengers_intl integer) '
+								   'WITHOUT ROWID;')
 
-# Commit changes and close the connection to the database file.
-connection.commit()
-connection.close()
+  servers_table_create_query = ('CREATE TABLE servers ('
+							      'id integer PRIMARY KEY, '
+								  'subsection text, '
+								  '\'0-4\' integer, '
+								  '\'4-8\' integer, '
+								  '\'8-12\' integer, '
+								  '\'12-16\' integer, '
+								  '\'16-20\' integer, '
+								  '\'20-24\' integer) '
+								   'WITHOUT ROWID;')
 
+  # Create new SQLite tables.
+  cursor.execute(arrivals_table_create_query)
+  cursor.execute(servers_table_create_query)
+
+  # Use Pandas to commit a CSV to the database with an intermediate.
+  df_arrivals = pandas.read_csv(arrival_schedule_csvfile)
+  df_servers = pandas.read_csv(server_schedule_csvfile)
+
+  df_arrivals.to_sql("arrivals", connection, if_exists='append', index=False)
+  df_servers.to_sql("servers", connection, if_exists='append', index=False)
+
+  # Commit changes and close the connection to the database file.
+  connection.commit()
+  connection.close()
+
+
+def main():
+  """
+  Main.
+
+  Args:
+    None
+  """
+  # Build it!
+  build (sqlite_file, arrival_schedule_csvfile, server_schedule_csvfile)
+
+
+if __name__ == "__main__":
+  main()
 
