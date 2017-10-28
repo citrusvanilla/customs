7f5388a981a9a9a3dd187f1ae1ae2b6fc5f7eaae
Justin Fung <citrusvanilla@gmail.com>
2017-10-28 15:56:13 -0700

    refactor: update scrapper

--- customs_scrapper.py
+++ customs_scrapper.py
@@ -7,13 +7,25 @@
 ##
 ## ====================================================================
 """
+Adds a table of airplane arrivals to JFK from the JFK website to an
+SQLite database. Can be invoked from the command line through the
+following:
+
+> python customs_scraper.py
+
+Usage:
+  Please see README for how to compile the program and run the
+  model and data formatting requirements.
+
 """
 # pylint: disable=bad-indentation
 from __future__ import print_function
 
 import re
 import sqlite3
+import time
 import requests
+
 from bs4 import BeautifulSoup
 import html5lib
 
@@ -23,8 +35,10 @@ import html5lib
 customs_db = 'customs_db_test.sqlite'
 
 # List of URLs of pages we want to scrape.
-urls = ["https://www.airport-jfk.com/arrivals.php?tp=" + i
-                                for i in ["0", "6", "12", "18"]]
+urls = ["https://www.airport-jfk.com/arrivals.php?tp=0",
+        "https://www.airport-jfk.com/arrivals.php?tp=6",
+        "https://www.airport-jfk.com/arrivals.php?tp=12",
+        "https://www.airport-jfk.com/arrivals.php?tp=18"]
 
 # Define the DIV ids for the attributes of interest.
 flight_divs = {'parent_div': 'flight_detail',
@@ -33,6 +47,15 @@ flight_divs = {'parent_div': 'flight_detail',
                'airline': 'fair',
                'flight_num': 'fnum',
                'terminal': 'fterm_mob'}
+
+# Define Metadata for the new table.
+arrivals_table_create_query = ('CREATE TABLE arrivals ('
+                                 'origin text, '
+                                 'airport_code text, '
+                                 'arrival_time text, '
+                                 'airline text, '
+                                 'flight_num text,'
+                                 'terminal int);')
 
 # Define a template SQLite insertion query.
 insertion_query = ('INSERT INTO arrivals ('
@@ -54,18 +77,45 @@ insertion_query = ('INSERT INTO arrivals ('
 
 class CleanExtractAndVerify(object):
   """
+  Handler Class for cleaning, extracting, and verifying desired arrivals
+  database attributes.  Uses re library.
+
+  Member Data:
+    self.airport_code_match: regexp string for extracting airport codes
+    self.terminal_match: regexp string for extracting JFK terminal number
+    self.time_stamp_match: regexp string for extracting arrival time
+    self.origin_ver: regexp string for verifying extracted origin
+    self.flight_num_ver: regexp string for verifying extracted flight
+                         number
+
+  Member Functions:
+    origin: clean, extract, and verify html origin match
+    airport_code: clean, extract, and verify html airport code match
+    arrival_time: clean, extract, and verify html arrival time match
+    airline: clean, extract, and verify html airline match
+    flight_num: clean, extract, and verify html flight number match
+    terminal: clean, extract, and verify html terminal match
   """
   def __init__(self):
     """
+    CleanExtractAndVerify Class initializer.  Define regexp here.
     """
     self.airport_code_match = '\([A-Z]{3}\)'
     self.terminal_match = '[0-9]{1}'
-    self.time_stamp_match ='[0-9]{1,2}:[0-9]{2}'
-    self.origin_ver = '[^ a-zA-Z.]'
+    self.time_stamp_match = '[0-9]{1,2}:[0-9]{2}'
+    self.origin_ver = '[^ /a-zA-Z.]'
     self.flight_num_ver = '[^ A-Z0-9.]'
 
   def origin(self, result_set):
     """
+    CleanExtractAndVerify Class function for cleaning, extracting, and
+    verifying html origin matches.
+
+    Args:
+      result_set: a bs4.element.Tag object
+
+    Returns:
+      rtn.strip(): string
     """
     # Verify search result.
     if result_set is None: return
@@ -81,6 +131,14 @@ class CleanExtractAndVerify(object):
 
   def airport_code(self, result_set):
     """
+    CleanExtractAndVerify Class function for cleaning, extracting, and
+    verifying html airport code matches.
+
+    Args:
+      result_set: a bs4.element.Tag object
+
+    Returns:
+      rtn.strip(): string
     """
     # Verify search result.
     if result_set is None: return
@@ -94,6 +152,14 @@ class CleanExtractAndVerify(object):
 
   def arrival_time(self, result_set):
     """
+    CleanExtractAndVerify Class function for cleaning, extracting, and
+    verifying html arrival time matches.
+
+    Args:
+      result_set: a bs4.element.Tag object
+
+    Returns:
+      time_stamp.strip(): string
     """
     # Verify search result.
     if result_set is None: return
@@ -108,10 +174,21 @@ class CleanExtractAndVerify(object):
     if re.search('am', result_set.text, flags=re.IGNORECASE) and time_stamp[0:2] == '12':
         time_stamp = '00' + time_stamp[2:]
 
+    if re.search('pm', result_set.text, flags=re.IGNORECASE):
+        time_stamp = str(int(time_stamp[0:2])+12) + time_stamp[2:]
+
     return time_stamp.strip()
 
   def airline(self, result_set):
     """
+    CleanExtractAndVerify Class function for cleaning, extracting, and
+    verifying html airline matches.
+
+    Args:
+      result_set: a bs4.element.Tag object
+
+    Returns:
+      rtn: string
     """
     # Verify result.
     if result_set is None: return
@@ -123,6 +200,14 @@ class CleanExtractAndVerify(object):
 
   def flight_num(self, result_set):
     """
+    CleanExtractAndVerify Class function for cleaning, extracting, and
+    verifying html flight number matches.
+
+    Args:
+      result_set: a bs4.element.Tag object
+
+    Returns:
+      rtn: string
     """
     # Verify result.
     if result_set is None: return
@@ -130,10 +215,21 @@ class CleanExtractAndVerify(object):
     # Verify regular expression.
     if re.search(self.flight_num_ver, result_set.text): return
 
-    return result_set.text.strip()
+    # Clean
+    rtn = result_set.text.strip()
+
+    return rtn
 
   def terminal(self, result_set):
     """
+    CleanExtractAndVerify Class function for cleaning, extracting, and
+    verifying html terminal matches.
+
+    Args:
+      result_set: a bs4.element.Tag object
+
+    Returns:
+      rtn.strip(): string
     """
     # Verify result.
     if result_set is None: return
@@ -150,33 +246,47 @@ class CleanExtractAndVerify(object):
 
 def create_arrivals_table(database):
   """
+  Function for creating a table in SQLite.
+
+  Args:
+    database: a string representing database filename.
+
+  Returns:
+    VOID
   """
+  # Open a connection to the database.
   connection = sqlite3.connect(database)
   cursor = connection.cursor()
 
-  # Define Metadata for the tables.
-  arrivals_table_create_query = ('CREATE TABLE arrivals ('
-                                   'origin text, '
-                                   'airport_code text, '
-                                   'arrival_time text, '
-                                   'airline text, '
-                                   'flight_num text,'
-                                   'terminal int);')
-
+  # Build the table according to the SQLite query.
   cursor.execute(arrivals_table_create_query)
+
+  # Commit chnages and clean up resources.
   connection.commit()
   connection.close()
 
 def scrape_arrivals(database, urls):
   """
+  The main function for scraping the JFK arrivals website of arrivals.
+  Uses re, requets, time, sqlite3, html5lib, and BeautifulSoup libraries.
+
+  Args:
+    database: string representing database filename
+    urls: list of URLs to scrape
+
+  Returns:
+    VOID
   """
+  # Update the user.
+  print ("Connection to DB established.  Scraping ", len(urls),
+         " URLs into ", customs_db, "...\n", sep="")
+  start = time.time()
+
   # Open a connection to the database.
   connection = sqlite3.connect(database)
   cursor = connection.cursor()
-  print ("Connection to DB established.  Scraping ", len(urls),
-         " URLs into ", customs_db, "...", sep="")
 
-  # Initialize a CleanAndExtract class.
+  # Initialize a CleanExtractAndVerify class for the flight attributes.
   cleaner = CleanExtractAndVerify()
 
   # Initialize some counters for status updates.
@@ -184,19 +294,20 @@ def scrape_arrivals(database, urls):
   loaded_flights = 0
 
   # Scrape one url at a time.
-  for idx, url in enumerate(urls):
+  for url_num, url in enumerate(urls):
 
     # Send GET requrest to url and clean up soup with BeautifulSoup.
     request = requests.get(url)
     html = request.text
     soup = BeautifulSoup(html, "html5lib")
 
-    # Extract flight parent DIVs.
+    # Extract flight parent DIV.
     flights = soup.findAll(id=flight_divs['parent_div'])
 
     # Loop through all results of the parent DIV.
     num_flights = len(flights)
-    print ("Found ", num_flights, " records in URL #", idx+1, "...", sep="")
+    url_flights = 0
+    print ("Found ", num_flights, " records in URL #", url_num+1, "...", sep="")
 
     for record_num, flight in enumerate(flights):
 
@@ -215,7 +326,7 @@ def scrape_arrivals(database, urls):
       flight_num_result = flight.find(id=flight_divs['flight_num'])
       terminal_result = flight.find(id=flight_divs['terminal'])
 
-      # Use the CleanAndExtract class to return desired text.
+      # Use the CleanExtractAndVerify class to return desired text.
       flight_attrs['origin'] = cleaner.origin(origin_result)
       flight_attrs['airport_code'] = cleaner.airport_code(origin_result)
       flight_attrs['arrival_time'] = cleaner.arrival_time(arrival_time_result)
@@ -223,7 +334,7 @@ def scrape_arrivals(database, urls):
       flight_attrs['flight_num'] = cleaner.flight_num(flight_num_result)
       flight_attrs['terminal'] = cleaner.terminal(terminal_result)
 
-      # Skip the result entirely if we are missing any of the following:
+      # Skip the database insertion if we are missing attributes.
       if None in flight_attrs.values():
         bad_data += 1
         continue
@@ -240,23 +351,35 @@ def scrape_arrivals(database, urls):
 
       # Status update
       loaded_flights += 1
+      url_flights += 1
       if loaded_flights % 100 == 0:
-        print ("Loaded ", loaded_flights, " flights into ",
-               customs_db, "...", sep="")
+        print ("Loaded ", loaded_flights,
+               " total flights so far into the database...", sep="")
 
     # End of URL.
-    print ("URL #", idx+1," loaded. ", bad_data, " records discarded in total.", sep="")
+    print (url_flights, " records in URL #", url_num+1, " loaded.\n", sep="")
 
   # Clean-up resources.
   connection.commit()
   connection.close()
 
-  print ("Scraping complete. ", loaded_flights, " good records loaded.", sep="")
+  # Write out scraping performance.
+  print ("Scraping completed.\n", loaded_flights, " good records loaded.\n",
+         bad_data, " bad records discarded.\n", "Total time: ",
+         int(time.time()-start), " seconds.\n", sep="")
 
 ## ====================================================================
 
 def main():
   """
+  Main.  Invoked from command line with no arguments.  Creates table in
+  SQLite database and inserts web-scrapped data into the database.
+
+  Args:
+    None
+  
+  Returns:
+    VOID
   """
   # Create table.
   create_arrivals_table(customs_db)
