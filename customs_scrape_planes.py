ad4e258ace1968f5aa0ff8375267af1ca806142d
Justin Fung <citrusvanilla@gmail.com>
2017-10-29 17:48:14 -0700

    documentation: added DOCS to plane scrapper, and linted file

--- customs_scrape_planes.py
+++ customs_scrape_planes.py
@@ -6,6 +6,8 @@
 ##  Copyright 2017 Justin Fung. All rights reserved.
 ##
 ## ====================================================================
+# pylint: disable=bad-indentation,bad-continuation,multiple-statements
+# pylint: disable=invalid-name
 """
 Adds a table of airplanes with carrier, aircraft, and number of seats
 to an SQLite database. Can be invoked from the command line through the
@@ -18,23 +20,24 @@ Usage:
   model and data formatting requirements.
 
 Helpful Debugging Commands:
-  driver.save_screenshot('image.png') -> screenshots your driver's 
-  										 current screen.
+  driver.save_screenshot('image.png') -> screenshots your driver's
+                                         current screen.
+
+Performance:
+
+
+TODO:
+  - complete the fill_search_form_and_submit error handling
 """
 from __future__ import print_function
 
+from datetime import datetime
 import re
-import selenium
-from bs4 import BeautifulSoup
-import html5lib
 import sqlite3
 
-from datetime import datetime
 from selenium import webdriver
 
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
+import html5lib
 
 
 ## ====================================================================
@@ -49,7 +52,7 @@ webdriver_exe = '/usr/local/phantomjs-2.1.1-macosx/bin/phantomjs'
 # URL we want to scrape.
 url = "https://www.seatguru.com/findseatmap/findseatmap.php"
 
-# SQLite query for creating a planes table in an SQLite database.
+# SQLite query for creating a 'planes' table in an SQLite database.
 create_planes_table_query = ('CREATE TABLE planes ('
                                'carrier text, '
                                'aircraft text, '
@@ -68,10 +71,33 @@ insertion_query = ('INSERT INTO planes ('
 
 ## ====================================================================
 
-class PlaneScraper(object):
-	pass
 
-## ====================================================================
+def load_driver(webdriver_exe, url):
+  """
+  Starts up a webdriver and loads a passed URL.
+
+  Args:
+    webdriver_exe: filepath to webdriver binary/executable
+    url: URL address as string
+
+  Returns:
+    driver: a initialized selenium webdriver object
+  """
+  # Use PhantomJS headless browser.
+  driver = webdriver.PhantomJS(webdriver_exe)
+
+  # Default implicit WAIT command for all AJAX and JS loading.
+  driver.implicitly_wait(5) # seconds
+
+  # Retrieve the page.
+  driver.get(url)
+
+  # Status update.
+  print("PhantomJS driver \'", webdriver_exe, " \' initialized.\n ",
+        "Driver has loaded \'", url, "\'.\n", sep="")
+
+  # Return driver control to main.
+  return driver
 
 
 def create_planes_table(database):
@@ -89,7 +115,7 @@ def create_planes_table(database):
   cursor = connection.cursor()
 
   # TROUBLESHOOTING
-  cursor_arrivals.execute('DROP TABLE IF EXISTS planes;')
+  cursor.execute('DROP TABLE IF EXISTS planes;')
 
   # Build the table according to the SQLite query.
   cursor.execute(create_planes_table_query)
@@ -99,28 +125,16 @@ def create_planes_table(database):
   connection.close()
 
 
-def load_driver(webdriver_exe, url):
-  """
-  """
-  # Use PhantomJS headless browser.
-  driver = webdriver.PhantomJS(webdriver_exe)
-  
-  # Default implicit WAIT command for all AJAX and JS loading.
-  driver.implicitly_wait(5) # seconds
-
-  # Retrieve the page.
-  driver.get(url)
-
-  # Status update.
-  print("PhantomJS driver \'", webdriver_exe, " \' initialized.\n ",
-  	    "Driver has loaded \'", url, "\'.\n", sep="")
-
-  # Return driver control to main.
-  return driver
-
-
 def fill_search_form_and_submit(driver, flight_attrs):
   """
+  Completes the main search form at SeatGuru.com and submit it.
+
+  Args:
+    driver: an initialized selenium webdriver object
+    flight_attrs: a dictionary containing attributes for the form
+
+  Returns:
+    success: boolean indicating presence of successful search result
   """
   # boolean for success
   success = True
@@ -130,14 +144,15 @@ def fill_search_form_and_submit(driver, flight_attrs):
   airline_field.clear()
   carrier_name = flight_attrs['carrier'].split(" ")
   airline_field.send_keys(" ".join(carrier_name))
-  airline_autocomplete = driver.find_elements_by_css_selector(".ui-autocomplete.ui-menu.ui-widget.ui-widget-content.ui-corner-all")
+  airline_autocomplete = driver.find_elements_by_css_selector(
+           ".ui-autocomplete.ui-menu.ui-widget.ui-widget-content.ui-corner-all")
 
   # If the database carrier name has not triggered the autocomplete form,
   # we should shorten the name and try again.
   while not airline_autocomplete.is_displayed() and len(carrier_name) > 1:
-    airline.clear()
+    airline_field.clear()
     carrier_name = carrier_name[0: len(carrier_name) - 1]
-    airline.send_keys(" ".join(carrier_name))
+    airline_field.send_keys(" ".join(carrier_name))
 
   # If we triggered the airline autocomplete, choose the top result.
   # If we could not trigger the autocomplete, we must return False.
@@ -145,8 +160,8 @@ def fill_search_form_and_submit(driver, flight_attrs):
     driver.find_element_by_class_name('ui-autocomplete')\
                    .find_element_by_class_name('ui-corner-all').click()
   else:
-  	success = False
-  	return success
+    success = False
+    return success
 
   # Fill out the flight number on the front page next.
   flight_num = driver.find_element_by_id('flightno')
@@ -165,49 +180,68 @@ def fill_search_form_and_submit(driver, flight_attrs):
   # Control should now be looking at a results page.
   # If results are not present, go back and try next record.
   if len(driver.find_elements_by_class_name('chooseFlights-row')) == 0:
-  	  success = False
-  	  return success
+      success = False
+      return success
 
   return success
 
 
-def get_indices(cursor):
+def _get_indices(cursor):
   """
+  Helper function for retrieving the indices from the Arrivals table
+  of the headers of interest.
+
+  Args:
+    cursor: an sqlite cursor initialized from the customs database
+
+  Returns:
+    indices: the indices of the headers of interest
   """
   # Get the indices from the arrivals table that we need.
   headers = cursor.execute('PRAGMA table_info(arrivals)').fetchall()
-  
+
   # Initialize a dictionary to hold relevant indexes.
   indices = {'origin': None,
-  			 'airline': None,
-  			 'flight_num': None,
-  			 'arrival_time': None}
-  			 #'date': None}
+         'airline': None,
+         'flight_num': None,
+         'arrival_time': None}
+         #'date': None}
 
   # Find the indices and store them.
   for idx, tup in enumerate(headers):
-  	if tup[1] == "origin":
-  		indices['origin'] = idx
-  	elif tup[1] == "airline":
-  		indices['airline'] = idx
-  	elif tup[1] == "flight_num":
-  		indices['flight_num']= idx
-  	elif tup[1] == "arrival_time":
-  		indices['arrival_time'] = idx
-  	#elif tup[1] == "date":
-  	#	indices['date'] = idx
+    if tup[1] == "origin":
+      indices['origin'] = idx
+    elif tup[1] == "airline":
+      indices['airline'] = idx
+    elif tup[1] == "flight_num":
+      indices['flight_num'] = idx
+    elif tup[1] == "arrival_time":
+      indices['arrival_time'] = idx
+    #elif tup[1] == "date":
+    # indices['date'] = idx
 
   # Exit if these indices are not found.
   if None in indices.values():
-  	raise Exception('Could not match arrivals table headers. '
-  		            'Check header names.')
+    raise Exception('Could not match arrivals table headers. '
+                  'Check header names.')
 
   # Return the indices dictionary.
   return indices
 
 
-def extract_plane_insert_and_return(driver, flight_attrs):
+def extract_plane_insert_and_return(driver, cursor_planes, flight_attrs):
   """
+  Extracts plane data from the plane's page.  Inserts plane data into
+  the customes database.
+
+  Args:
+    driver: an initialized selenium webdriver
+    cursor_planes: a secondary initialized sqlite cursor to operate on
+                   the plane table
+    flight_attrs: a dictionary to store intermediate plane database
+
+  Returns:
+    success: a boolean indicating successful insertion of plane data
   """
   # Boolean for success.
   success = False
@@ -218,14 +252,15 @@ def extract_plane_insert_and_return(driver, flight_attrs):
 
   # Check to see if this entry is already in the database.
   cursor_planes.execute('SELECT * FROM planes '
-    					  'WHERE carrier = \'{carrier}\' '
-    					    'AND aircraft = \'{aircraft}\';'\
-    							        .format(
+                'WHERE carrier = \'{carrier}\' '
+                  'AND aircraft = \'{aircraft}\';'\
+                          .format(
                                              carrier=flight_attrs['carrier'],
-                                             aircraft=flight_attrs['aircraft'])
-  
+                                             aircraft=flight_attrs['aircraft']))
+
   # If yes, return false.
-  if len(cursor_planes.fetchall()) is != 0: return success
+  if len(cursor_planes.fetchall()) != 0:
+    return success
 
   # Some airplane elements are not clickable.  Check and return false if so.
   if not airplane.is_enabled(): return success
@@ -241,15 +276,15 @@ def extract_plane_insert_and_return(driver, flight_attrs):
 
   for item in seat_list:
     if re.search('[0-9]{1,}', item.text):
-	  num_seats += int(re.compile('[0-9]{1,}').search(item.text).group(0))
-  
+      num_seats += int(re.compile('[0-9]{1,}').search(item.text).group(0))
+
   flight_attrs['total_seats'] = num_seats
-  
+
   # Insert into database.
   cursor_planes.execute(insertion_query.format(
-    										carrier=pass,
-    										aircraft=pass,
-    										total_seats=pass))
+                        carrier=flight_attrs['carrier'],
+                        aircraft=flight_attrs['aircraft'],
+                        total_seats=flight_attrs['total_seats']))
   # Return contol.
   driver.execute_script("window.history.go(-1)")
   success = True
@@ -259,6 +294,14 @@ def extract_plane_insert_and_return(driver, flight_attrs):
 
 def scrape_planes(driver, database):
   """
+  Main routine for scraping plane data into an SQLite database.
+
+  Args:
+    driver: an initialized selenium webdriver object
+    database: filepath to the customs database
+
+  Returns:
+    VOID
   """
   # Open up a connection to the database, initialize a cursor.
   connection = sqlite3.connect(database)
@@ -266,41 +309,41 @@ def scrape_planes(driver, database):
   cursor_planes = connection.cursor()
 
   # get indices from database.
-  indices = get_indices(cursor_arrivals)
+  indices = _get_indices(cursor_arrivals)
 
   # Point the cursor at the list of flights for which we want plane data.
-  cursor.execute('SELECT * FROM arrivals')
+  cursor_arrivals.execute('SELECT * FROM arrivals')
 
   # SOME COUNTERS
   inserted_planes = 0
   bad_planes = 0
 
   # Iterate through every flight.
-  for idx, row in enumerate(cursor):
+  for idx, row in enumerate(cursor_arrivals):
 
     # Initiate a dictionary of flight attributes for the web scraping routine.
     flight_attrs = {'carrier': None,
-  			 		'flight_num': None,
-  			 		'date': None,
-  			 		'arrival_time': None,
-  			 		'aircraft': None,
-  			 		'total_seats': None}
+            'flight_num': None,
+            'date': None,
+            'arrival_time': None,
+            'aircraft': None,
+            'total_seats': None}
 
-  	# Retrieve the carrier, flight number, arrival_time and date from the sqlite query.
-  	flight_attrs['carrier'] = row[indices['airline']]
-  	flight_attrs['flight_num'] = row[indices['flight_num']]
-  	#flight_attrs['date'] = row[indices['date']]
-  	flight_attrs['arrival_time'] = row[indices['arrival_time']]
+    # Retrieve the carrier, flight number, arrival_time and date from the sqlite query.
+    flight_attrs['carrier'] = row[indices['airline']]
+    flight_attrs['flight_num'] = row[indices['flight_num']]
+    #flight_attrs['date'] = row[indices['date']]
+    flight_attrs['arrival_time'] = row[indices['arrival_time']]
 
-  	# Fill out the search page.
-  	found_potential_plane = fill_search_form_and_submit(driver, flight_attrs)
+    # Fill out the search page.
+    found_potential_plane = fill_search_form_and_submit(driver, flight_attrs)
 
     # If our search did not turn up a result, we move to the next record.
     if not found_potential_plane: continue
 
     # If we have a potential plane, let's extract it, insert it into
     # the DB and return control to the search page.
-    extracted = extract_plane_insert_and_return(driver, flight_attrs)
+    extracted = extract_plane_insert_and_return(driver, cursor_planes, flight_attrs)
 
     # If we could not extract plane information, move to next record.
     if not extracted: continue
@@ -321,11 +364,11 @@ def scrape_planes(driver, database):
 
 def main():
   """
-  Main.
+  Main.  Invoked from command line.
 
   Args:
     None
-  
+
   Returns:
     VOID
   """
@@ -338,9 +381,6 @@ def main():
   # Initiate Scraping.
   scrape_planes(driver, customs_db)
 
-  return
 
-
-if __name__ == "__main__": 
+if __name__ == "__main__":
   main()
-
